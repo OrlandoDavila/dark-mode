9e95c38ed8bb47d890ff84126e70cf34909bca01
Orlando Davila <odavila0308@gmail.com>
2020-08-05 16:16:17 -0400

    MVP complete

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,14 @@
+  
+import {useEffect} from 'react'
+import useLocalStorage from './useLocalStorage'
+
+function useDarkMode(){
+   const [darkMode, setDarkMode] = useLocalStorage()
+
+   useEffect(()=>{
+       document.body.classList.toggle('dark-mode')
+   },[darkMode])
+   return [darkMode, setDarkMode]
+}
+
+export default useDarkMode
